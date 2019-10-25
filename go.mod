c8237f1dd48d3877c8240d02aa15638a55311a90
Lyt99 <lyt1999131@163.com>
2019-10-25 19:02:05 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gqlregister
+
+go 1.12
+
+require (
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/graphql-go/graphql v0.7.8
+	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
+)
---
+++ register.go
@@ -0,0 +1,128 @@
+package gqlregister
+
+import (
+	"errors"
+	"fmt"
+	"github.com/graphql-go/graphql"
+	"reflect"
+)
+
+var (
+	ErrorNotStruct = errors.New("schema is not a struct")
+	ErrorColExists = errors.New("collection name has existed")
+)
+
+type MongoSession interface {
+	Insert(collection string, document interface{}) error
+	Delete(collection string, query interface{}) error
+	FindOne(collection string, query interface{}, t reflect.Type) (interface{}, error)
+	FindMany(collection string, query interface{}, t reflect.Type) ([]interface{}, error)
+	UpdateOne(collection string, query interface{}, update interface{}) error
+	UpdateMany(collection string, query interface{}, update interface{}) error
+	Close()
+}
+
+type SessionGetter interface {
+	GetSession() MongoSession
+}
+
+type TObjectPair struct {
+	T reflect.Type
+	Object *graphql.Object
+	Args graphql.FieldConfigArgument
+}
+type GraphqlMongoRegister struct {
+	collectionType map[string]TObjectPair
+	sessionGetter  SessionGetter
+	query          *graphql.Object
+	GraphiQL       bool
+	Pretty         bool
+}
+
+func New(sessionGetter SessionGetter) *GraphqlMongoRegister {
+	ret :=  &GraphqlMongoRegister{
+		collectionType: make(map[string]TObjectPair),
+		sessionGetter:  sessionGetter,
+		query:          &graphql.Object{},
+	}
+
+	return ret
+}
+
+func (m *GraphqlMongoRegister) makeQuery() {
+	fields := graphql.Fields{}
+
+	for k, v := range m.collectionType {
+		fields[k] = &graphql.Field{
+			Name:              k,
+			Type:              graphql.NewList(v.Object),
+			Args:              v.Args,
+			Resolve: func(p graphql.ResolveParams) (i interface{}, err error) {
+				session := m.sessionGetter.GetSession()
+				return session.FindMany(k, p.Args, v.T)
+			},
+			DeprecationReason: "",
+			Description:       v.Object.Description(),
+		}
+	}
+
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+
+	m.query = graphql.NewObject(rootQuery)
+}
+
+func (m *GraphqlMongoRegister) makeMutation() {
+	// TODO
+}
+
+func (m *GraphqlMongoRegister) GetSchema() (*graphql.Schema, error) {
+	m.makeQuery()
+
+	schemaConfig := graphql.SchemaConfig{
+		Query:        m.query,
+		Mutation:     nil,
+		Subscription: nil,
+		Types:        nil,
+		Directives:   nil,
+		Extensions:   nil,
+	}
+
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &schema, nil
+}
+
+func (m *GraphqlMongoRegister) Register(colName string, schema interface{}) (err error) {
+	defer func() {
+		if e := recover(); err != nil {
+			if err1, ok := e.(error); ok {
+				err = err1
+			} else {
+				panic(e)
+			}
+		}
+	}()
+
+	// colName是否已经存在
+	if _, exists := m.collectionType[colName]; exists {
+		return ErrorColExists
+	}
+
+	m.collectionType[colName] = TObjectPair{
+		T:      reflect.TypeOf(schema),
+		Object: graphql.NewObject(graphql.ObjectConfig{
+			Name:        colName,
+			Interfaces:  nil,
+			Fields:      BindFields(schema),
+			IsTypeOf:    nil,
+			Description: fmt.Sprintf("Struct in collection %s", colName),
+		}),
+		Args: BindArg(schema),
+	}
+
+
+	return nil
+}
